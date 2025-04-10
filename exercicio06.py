405e2c5e7cfb5512e2013f5701f443f69f7d30af
Bernard <bernardd.lego274@gmail.com>
2025-04-10 11:12:16 -0300

    exercicio 06 corrigido pelo professor

--- exercicio06.py
+++ exercicio06.py
@@ -1,7 +1,6 @@
-media = 5
 soma = 0
-for x in range(media):
-    n1 = float(input("Dgite um valor: "))
+for x in range(5):
+    n1 = int(input("Dgite um valor: "))
     soma += n1
-num = soma / media
-print(f"a media aritimetica: {num}")
+media = soma/5
+print(f"a media aritimetica: {media}")
