e73cfaedba3843ca3ffce7fdba486b3173b56b68
Bernard <bernardd.lego274@gmail.com>
2025-04-10 11:18:38 -0300

    Exercicio 06 alterado pelo professor,  exercicio original entre aspas

--- exercicio06.py
+++ exercicio06.py
@@ -1,6 +1,16 @@
-soma = 0
+"""soma = 0
 for x in range(5):
     n1 = int(input("Dgite um valor: "))
     soma += n1
 media = soma/5
-print(f"a media aritimetica: {media}")
+print(f"a media aritimetica: {media}")"""
+
+#modificação para fazer com que o úsuario atribua o numero de valores a ser pedido
+
+valor = int(input("digite quantos valores vão ser calculados dentro da media: "))
+soma = 0
+for x in range(valor):
+    n1 = int(input("Dgite um valor: "))
+    soma += n1
+media = soma/valor
+print(f"a media aritimetica: {media: .1f}")
